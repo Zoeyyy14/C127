8cc4a3338f1b2eb108e36b61f61aae99a4ab1f0e
Zoeyyy14 <70824285+Zoeyyy14@users.noreply.github.com>
2021-08-04 17:53:08 -0500

    Add files via upload

---
+++ scraper.py
@@ -0,0 +1,32 @@
+from selenium import webdriver
+from bs4 import BeautifulSoup
+import csv
+import time
+
+start_url="https://exoplanets.nasa.gov/discovery/exoplanet-catalog/"
+browser=webdriver.Chrome("/Users/ZoeyMa/Downloads/C127/chromedriver")
+browser.get(start_url)
+time.sleep(2)
+def scrap():
+    headers=["name","light_years_from_earth","planet_mass","stellar_magnitute","discovery_date"]
+    planet_data=[]
+    for i in range(0,445):
+        soup=BeautifulSoup(browser.page_source,"html.parser")
+        for ul_tag in soup.find_all("ul",attrs={"class","exoplanet"}):
+            li_tags=ul_tag.find_all("li")
+            temp_list=[]
+            for index,li_tag in enumerate(li_tags):
+                if index==0:
+                    temp_list.append(li_tag.find_all("a")[0].contents[0])
+                else:
+                    try:
+                        temp_list.append(li_tag.contents[0])
+                    except:
+                        temp_list.append("")
+            planet_data.append(temp_list)
+        browser.find_element_by_xpath("/html/body/div[2]/div/div[3]/section[2]/div/section[2]/div/div/article/div/div[2]/footer/div/div/div/nav/span[2]").click()
+    with open("scraper.csv","w")as f:
+        csvwriter=csv.writer(f)
+        csvwriter.writerow(headers)
+        csvwriter.writerows(planet_data)
+scrap()
